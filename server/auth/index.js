1159ed332f98d9e782a19333b2c81ffdc70715a0
sheriffahmed <sheriff.v.ahmed@gmail.com>
2022-07-06 17:52:55 -0400

    testing auth route signup2

--- server/auth/index.js
+++ server/auth/index.js
@@ -13,9 +13,8 @@ router.post('/login', async (req, res, next) => {
 
 router.post('/signup', async (req, res, next) => {
   try {
-res.send("this is the sign up route")
-    // const user = await User.create(req.body)
-    // res.send({token: await user.generateToken()})
+    const user = await User.create(req.body)
+    res.send({token: await user.generateToken()})
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
       res.status(401).send('User already exists')
