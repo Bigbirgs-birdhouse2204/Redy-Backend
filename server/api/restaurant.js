9d970d83e5b239a04c085b59a68343d9cdba88a9
sheriffahmed <sheriff.v.ahmed@gmail.com>
2022-07-06 15:18:01 -0400

    adding express middleware for restaurant route

--- server/api/restaurant.js
+++ server/api/restaurant.js
@@ -7,7 +7,7 @@ module.exports = router;
 router.get('/', async (req, res, next) => {
   try {
     const restaurants = await Restaurant.findAll();
-    // res.send("Hello")
+
     res.json(restaurants);
   } catch (err) {
     next(err);
