c4462b32a8a7bf9c2442642724e47596ba119668
Nikul Patel <nikulp1997@gmail.com>
2022-07-07 13:03:34 -0400

    res routes

--- server/api/maps.js
+++ server/api/maps.js
@@ -5,23 +5,22 @@ const router = require('express').Router();
 const axios = require('axios');
 const Restaurant = require('../db/models/Restaurant');
 module.exports = router;
+require('dotenv').config();
 
 router.get('/', async (req, res, next) => {
   try {
-
     // const lat = req.body.lat
     // const long = req.body.long
     const lat = 40.714184;
     const long = -74.006238;
     let config = {
-      method: "get",
+      method: 'get',
       url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${long}&radius=1609&type=restaurant&key=${process.env.API_KEY}`,
       headers: {},
     };
-   const {data} = await axios(config);
+    const { data } = await axios(config);
 
-   const maps = data.results.map(restaurant =>
-    {
+    const maps = data.results.map((restaurant) => {
       return {
         name: restaurant.name,
         address: restaurant.vicinity,
@@ -32,13 +31,10 @@ router.get('/', async (req, res, next) => {
         imgUrl: restaurant.photos[0].photo_reference,
         longitude: restaurant.geometry.location.lng,
         latitude: restaurant.geometry.location.lat,
-
-      }
-
-    }
-    )
-  //  res.json(data)
-    res.json(maps)
+      };
+    });
+    //  res.json(data)
+    res.json(maps);
 
     // const restaurants = await Restaurant.findAll();
     // res.json(restaurants);
