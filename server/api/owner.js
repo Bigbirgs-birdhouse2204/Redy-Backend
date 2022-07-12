f4e13deda60e988577315d2433dbd8deb53d6b62
sheriffahmed <sheriff.v.ahmed@gmail.com>
2022-07-12 12:49:10 -0400

    added owner api Routes to display an Owner's restaurants

---
+++ server/api/owner.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const {
+  models: { User, RestaurantOwner, Restaurant },
+} = require('../db');
+module.exports = router;
+
+router.get('/restaurants', async (req, res, next) => {
+  try {
+
+    const user = await User.findByToken(req.headers.authorization)
+    let owner = RestaurantOwner.findOne({where: {userId: user.id}});
+
+    const restaurants = await Restaurant.findAll({where: {
+      ownerId: owner.id
+      // attributes: ['id', 'email', 'firstName', 'lastName', 'phone']
+    }});
+    res.json(restaurants);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const singleRestaurant = await Restaurant.findByPk(req.params.id);
+
+    res.json(singleRestaurant);
+  } catch (err) {
+    next(err);
+  }
+});
