beba14e31bd86d6a634195ec4a16a334bb58fe95
Simon <simon.jin.chen@gmail.com>
2022-07-18 16:45:17 -0400

    added reservation route for user

--- server/api/reservation.js
+++ server/api/reservation.js
@@ -1,36 +1,38 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const {
-  models: { Reservation, DiningTable, ReservedSeating, User, Restaurant  },
-} = require('../db');
+  models: { Reservation, DiningTable, ReservedSeating, User, Restaurant },
+} = require("../db");
 module.exports = router;
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const reservation = await Reservation.findAll({include: DiningTable});
+    const reservation = await Reservation.findAll({ include: DiningTable });
     res.json(reservation);
   } catch (err) {
     next(err);
   }
 });
 
-router.get('/business/:restaurantId', async (req, res, next) => {
+router.get("/business/:restaurantId", async (req, res, next) => {
   try {
     const reservation = await Reservation.findAll({
       where: {
-        restaurantId: req.params.restaurantId
+        restaurantId: req.params.restaurantId,
       },
-      include: [DiningTable, {
-        model: User,
-        attributes:['firstName', 'lastName', 'phone']
-
-      }]
+      include: [
+        DiningTable,
+        {
+          model: User,
+          attributes: ["firstName", "lastName", "phone"],
+        },
+      ],
     });
     res.json(reservation);
   } catch (err) {
     next(err);
   }
 });
-router.get('/:id', async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   try {
     const singleRestaurant = await Reservation.findByPk(req.params.id);
 
@@ -39,54 +41,54 @@ router.get('/:id', async (req, res, next) => {
     next(err);
   }
 });
-router.post('/', async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   try {
+    const user = await User.findByPk(req.body.userId);
+    const restaurant = await Restaurant.findByPk(req.body.restaurantId);
 
-     const user = await User.findByPk(req.body.userId)
-     const restaurant = await Restaurant.findByPk(req.body.restaurantId)
-
-const reservation = await Reservation.findOne({where:{
-  userId: req.body.userId
-},
-include: DiningTable
-});
- if(reservation){
-const occupiedSeat = await ReservedSeating.findOne({where: {
-  reservationId: reservation.id
-}} )
-if(occupiedSeat){
-
-  const oldDiningTable = await DiningTable.findOne({where:{
-    id: occupiedSeat.diningTableId,
-
-  }});
-  if(oldDiningTable){
-    await oldDiningTable.update({isOccupied: false});
-    await oldDiningTable.save();
-    await ReservedSeating.destroy({
+    const reservation = await Reservation.findOne({
       where: {
-        reservationId: reservation.id,
-        diningTableId: oldDiningTable.id,
+        userId: req.body.userId,
       },
+      include: DiningTable,
     });
-  }
-}
-
-await Reservation.destroy({
-  where: {
-    userId: user.id,
-  },
-});
-
- }
-
-
-    const diningTable = await DiningTable.findByPk(req.body.diningTableId)
-    const newReservation =  await user.createReservation({
-        status: req.body.status,
-        partySize: req.body.partySize,
+    if (reservation) {
+      const occupiedSeat = await ReservedSeating.findOne({
+        where: {
+          reservationId: reservation.id,
+        },
       });
-    await newReservation.setRestaurant(restaurant)
+      if (occupiedSeat) {
+        const oldDiningTable = await DiningTable.findOne({
+          where: {
+            id: occupiedSeat.diningTableId,
+          },
+        });
+        if (oldDiningTable) {
+          await oldDiningTable.update({ isOccupied: false });
+          await oldDiningTable.save();
+          await ReservedSeating.destroy({
+            where: {
+              reservationId: reservation.id,
+              diningTableId: oldDiningTable.id,
+            },
+          });
+        }
+      }
+
+      await Reservation.destroy({
+        where: {
+          userId: user.id,
+        },
+      });
+    }
+
+    const diningTable = await DiningTable.findByPk(req.body.diningTableId);
+    const newReservation = await user.createReservation({
+      status: req.body.status,
+      partySize: req.body.partySize,
+    });
+    await newReservation.setRestaurant(restaurant);
     await newReservation.addDiningTable(diningTable);
     res.status(201).send(newReservation);
   } catch (error) {
@@ -94,19 +96,30 @@ await Reservation.destroy({
   }
 });
 
-router.post('/waitlist', async (req, res, next) => {
+router.post("/waitlist", async (req, res, next) => {
   try {
-    const user = await User.findByPk(req.body.userId)
-    const restaurant = await Restaurant.findByPk(req.body.restaurantId)
-    const reservation =  await user.createReservation({
-        status: req.body.status,
-        partySize: req.body.partySize,
-      });
-    await reservation.setRestaurant(restaurant)
+    const user = await User.findByPk(req.body.userId);
+    const restaurant = await Restaurant.findByPk(req.body.restaurantId);
+    const reservation = await user.createReservation({
+      status: req.body.status,
+      partySize: req.body.partySize,
+    });
+    await reservation.setRestaurant(restaurant);
     res.status(201).send(reservation);
   } catch (error) {
     next(error);
   }
 });
 
+// this is the route to get all reservations for specific regular user
+router.get("/user/:userid", async (req, res, next) => {
+  try {
+    const reservations = await Reservation.findAll({
+      where: { userId: req.params.userid },
+    });
 
+    res.json(reservations);
+  } catch (err) {
+    next(err);
+  }
+});
